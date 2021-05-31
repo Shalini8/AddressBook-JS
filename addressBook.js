68848fccf28abdd0aa4efcb2f40eab40c4f0ff8f
Shalini8 <shalu8mar@gmail.com>
2021-05-31 07:35:00 +0530

    create AddressBookContact

--- addressBook.js
+++ addressBook.js
@@ -1,1 +1,25 @@
-console.log("welcome to addressBook")
+class AddressBookContact{
+    constructor (firstName, lastName, address, city, state, zipCode, phoneNumber, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    
+    toString() {
+        return "firstName="+this.firstName+" ,lastName="+this.lastName+
+        " ,address="+this.address+" ,city="+this.city+" ,state="+this.state+
+        " ,zip="+this.zip+ " ,phoneNumber="+this.phoneNumber+" ,email="+this.email;
+    }
+}
+
+
+let addressBook = new AddressBookContact("Shalini", "Pandey", "Asna", 
+    "jagdalpur", "Chhattisgarh", "494001", "9329000000", "s@gmail.com");
+
+console.log(addressBook.toString());
